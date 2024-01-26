8f13d710079622022d38d9096c6f59787dafcd3d
BonaR7702 <richibonanomi@gmail.com>
2024-01-26 17:01:11 +0100

    Update Script_homework_II.R
    sono stanco ciao

--- Script_homework_II.R
+++ Script_homework_II.R
@@ -17,7 +17,7 @@ options(tibble.width=Inf)
 fetal_Health <- tibble(read.csv("fetal_health.csv")) %>%
   mutate_at(vars(fetal_health),as.factor) # non è elegante da migliorare
 
-sum(fetal_Health$severe_decelerations != 0)# not normaly distributed most values are 0
+sum(fetal_Health$severe_decelerations != 0)# not normaly distributed most values are 0 (cos'è sta cosa????)
 hist(fetal_Health$histogram_number_of_zeroes) # è un conteggio dunque probabilmente si distribuisce come poisson
 hist(fetal_Health$histogram_number_of_peaks) # è un conteggio dunque probabilmente si distribuisce come poisson
 hist(fetal_Health$histogram_variance) # questo sembra distribuirsi come gamma
@@ -46,9 +46,11 @@ pca <- fetal_Health%>%
 pca$sdev 
 cumsum(pca$sdev^2/10) < 0.70 #4 componenti
 
-main_comp <- names(fetal_Health)[apply(pca$loadings[,1:4], 2, function(x) which(x**2==max(x**2)))]
+#selezioniamo le prime d variabili che sono normali e unite coprono il 60 o 70 % della variabilità
+(main_comp <- names(fetal_Health)[apply(pca$loadings[,1:4], 2, function(x) which(x**2==max(x**2)))])
 # da fare meglio
 
+#scatterplot con le prime 2 variabili più significative a seguito della pca
 fetal_Health %>%
   select(abnormal_short_term_variability, percentage_of_time_with_abnormal_long_term_variability, fetal_health) %>%
   ggplot(mapping = aes(x=abnormal_short_term_variability , y =percentage_of_time_with_abnormal_long_term_variability, color = fetal_health)) +
@@ -114,11 +116,22 @@ library(mclust)
 (fetal_Health_EM<-fetal_Health%>%
   select(all_of(main_comp))) #dataset solo con le variabili selezionate tramite pca
 
+set.seed(123) #col set.seed stima con k=3 (siccome facilmente sbaglia....probabilmente la scelta degli initial values è cruciale...
+#da specificare nell'homework)
 health_mclust_ICL<-mclustICL(fetal_Health_EM) #non ha fatto alcun salto (si può ipoptizzare che il numero di u.s. sia sufficiente
 #a stimare anche il modello più complkesso VVV anche con 9 gruppi....diu default mclust stima da 1 a 9 gruppi per i 14 modelli possibili)
 summary(health_mclust_ICL) #modello VEV con 3 componenti 
-plot(health_mclust_ICL)  #guardate che bellino 
+plot(health_mclust_ICL,ylim=c(-20000,20000))  #guardate che bellino (si vede che il modello EEV con più cluster sta prendendo una buona risalita)
 str(health_mclust_ICL)
+
+#per curiosità:
+set.seed(123)
+plot(mclustICL(fetal_Health_EM,G=2:21),ylim=c(0,50000)) #dopo k=18 sembra scendere quindi il modello VEV con 3 componenti è il migliore ma ci va bene solo con
+#accurati starting values (probabilmente 3 gruppi è raro che li colga accuratamente...motivazione: DA COMPLETARE)
+#qui si vede la difficoltà dell'EM probabilmente con gruppi molto vicini tra loro (DA ARGOMENTARE)
+#nessun salto nel fitting dei modelli (le u.s. sono sufficienti anche per k molto grande (con 4 variabili i parametri non sono eccessivi))
+#CURSE OF DIMENSION
+#da verificare questa cosa che stima tutti i modelli....
 
 
 set.seed(123)
@@ -166,3 +179,51 @@ precisione_EM<-classError(etichette_stimate, class=etichette)
 
 
 #poi in futuro voi geni se volete mettere le variabili in INGLESE è più bello
+
+#CLASSIFICAZIONE SECONDO MDA
+#creo un dataset come bozza x fare la classificazione (DA RIVEDERE LE VARIABILI DA USARE)
+(fetal_Health_bozza<-fetal_Health%>%
+  select(-c(starts_with("histogram"), severe_decelerations, fetal_health))) #se ricordo bene l'mda coglie 
+#le misture tra diverse variabili..non all'interno di una singola variabile che quindi viene richietsa normale
+#DA VERIFICARE
+etichette  
+
+
+#suddivisione train test
+set.seed(123)
+(index<-sample(c(T,F),size=n,replace=T,prob=c(0.7,0.3)))
+prop.table(table(index)) #ci sta
+
+#stima modello migliore + calcolo MER sul test set (STO SCHIFPO NON FUNZIONA)
+set.seed(123)
+(fetal_Health_bozza_train<-fetal_Health_EM[index,])
+(etichette_train<-fetal_Health[index,"fetal_health"])
+(fetal_Health_bozza_test<-fetal_Health_EM[ifelse(index==F,T,F),])
+(etichette_test<-fetal_Health[ifelse(index==F,T,F),"fetal_health"])
+mod = MclustDA(data=data.frame(fetal_Health_bozza_train), as.factor(etichette_train)) # sul primo si è soffermato sul 20%
+sum(predict(mod, fetal_Health_bozza_test)$class != etichette_test)
+class(data.frame(fetal_Health_bozza_train))
+
+#per il futuro un eventuale under/over sampling ha senso (probabilmente più under sampling dalla
+#categoria sano che è maggiormente presente rispetto alle altre 2)
+table(fetal_Health$fetal_health)
+# 1655 295 176
+
+#ha senso provare ad implementare un MDA basato sul MER con il quale:
+#60% training
+#20% selection (basata sul MER o sul BIC....col MER tocca implementarla)
+#20% testing 
+#con cui poi fare un cross-validation ripetendo 10 volte la selezione dei 3 sets e valutare la media dei MER 
+#per selezionare il miglior modello
+
+#provare una funzione che prende g1 g2 e g3 e mod1 mod2 e mod3 e restituisce un MER (facendo la media 
+#con 10 cross-validation)
+#si può implementare con un apply ma devo creare una matrice
+# con 6 variabili: g1 (numero gruppi nella mistura della prima etichetta) g2 g3 mod1 (tipo modello prima etichetta)
+#mod2 e mod3
+#rischio: che per ogni gruppo il più accurato sia sempre VVV con 5 cluster non avcendo penalizzazione se uso il MER invece
+#del BIC (o magari VVV con 5 gruppi va in overfitting e quindi su dati nuovi (del selection set) non worka)
+
+#ci penso lunedì
+
+#tocca  cambiare un po' i nomi se no palese che abbiamo copiato e incollato dallo script...
